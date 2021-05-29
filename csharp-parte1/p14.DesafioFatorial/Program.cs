3af99fcb340401073438da06536043b43dd54806
taohansens <taohansens@gmail.com>
2021-05-29 14:31:00 -0300

    Finish

---
+++ csharp-parte1/p14.DesafioFatorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace p14.DesafioFatorial {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Desafio Fatorial");
+            int fatorial = 1;
+
+            for (int i = 1; i < 11; i++) {
+                fatorial *= i;
+                Console.WriteLine("Fatorial de " + i + " = " + fatorial);
+            }
+        }
+    }
+}
