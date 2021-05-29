876782caaa29fe8ddb259c0561d12020dfbf3908
taohansens <taohansens@gmail.com>
2021-05-28 23:31:57 -0300

    Added

---
+++ csharp-parte1/4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4_ConversoesEOutrosTiposNumericos {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Executando projeto 4");
+            
+            double salario;
+            salario = 1200.40;
+
+            // int = valores até 32bits
+            int salarioEmInteiro;
+            salarioEmInteiro = (int)salario;
+            Console.WriteLine(salarioEmInteiro);
+
+            // long = variavel até 64bits
+            long idade;
+            idade = 13000000000000;
+            Console.WriteLine(idade);
+
+            // short = variavel 16bis
+            short quantidadeProdutos;
+            quantidadeProdutos = 15000;
+            Console.WriteLine(quantidadeProdutos);
+
+
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+        }
+    }
+}
---
+++ csharp-parte1/5-CaracteresETextos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _5_CaracteresETextos {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+            //character
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char) 65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char) (primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Banco do Brasil" + 2021;
+            string concaten = @"CSharp
+            - 2021 
+            - testando";
+            Console.WriteLine(titulo);
+            Console.WriteLine(concaten);
+
+
+
+
+
+
+
+        }
+    }
+}
