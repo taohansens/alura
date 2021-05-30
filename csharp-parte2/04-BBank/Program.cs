be16ab1b8f6cd9a898f2a7ebc03c1c200871ed43
taohansens <taohansens@gmail.com>
2021-05-30 18:39:36 -0300

    aula 03. metodo Sacar

---
+++ csharp-parte2/04-BBank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04_BBank {
+    class Program {
+        static void Main(string[] args) {
+            ContaCorrente contaGustavo = new ContaCorrente();
+            contaGustavo.titular = "Gustavo";
+            contaGustavo.agencia = 863;
+            contaGustavo.numero = 75421;
+            contaGustavo.saldo = 101.00;
+
+            Console.WriteLine("Saldo antes de sacar: " + contaGustavo.saldo);
+
+            double valorSacar = 80;
+            bool resultadoSaque = contaGustavo.Sacar(valorSacar);
+            Console.WriteLine("\nSacou " +  valorSacar +"? = " + resultadoSaque);
+
+            Console.WriteLine("\nSaldo após sacar:  " + contaGustavo.saldo);
+            
+            // segunda tentativa
+            resultadoSaque = contaGustavo.Sacar(valorSacar);
+            Console.WriteLine("\nSacou " + valorSacar + "? = " + resultadoSaque);
+
+            Console.WriteLine("\nSaldo após sacar:  " + contaGustavo.saldo);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- csharp-parte2/BBank/Program.cs
+++ csharp-parte2/BBank/Program.cs
@@ -3,8 +3,24 @@
 namespace BBank {
     class Program {
         static void Main(string[] args) {
-            new ContaCorrente();
 
+            ContaCorrente contaGustavo = new ContaCorrente();
+            contaGustavo.titular = "Gustavo";
+            contaGustavo.agencia = 863;
+            contaGustavo.numero = 75421;
+            contaGustavo.saldo = 101.10;
+
+            ContaCorrente contaGabriela = new ContaCorrente();
+            contaGabriela.titular = "Gustavo";
+            contaGabriela.agencia = 863;
+            contaGabriela.numero = 75421;
+            contaGabriela.saldo = 150.0;
+
+            Console.WriteLine($"{contaGustavo.titular}\n" +
+                $"Agência: {contaGustavo.agencia}\n" +
+                $"Número: {contaGustavo.numero}\n" +
+                $"Saldo: R${contaGustavo.agencia}");
+            Console.ReadLine();
         }
     }
 }
