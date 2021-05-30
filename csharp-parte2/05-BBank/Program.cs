1ff3c6881ab350edae869efeae5b1386c7d38df7
taohansens <taohansens@gmail.com>
2021-05-30 19:29:30 -0300

    aula 04. Nulos

---
+++ csharp-parte2/05-BBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05_BBank {
+    class Program {
+
+        static void Main(string[] args) {
+            string brkl = "==================";
+
+            Cliente gabriela = new Cliente();
+            gabriela.nome = "Gabriela";
+            gabriela.cpf = "784.455.145-22";
+            gabriela.profissao = "Estudante";
+
+            ContaCorrente conta = new ContaCorrente();
+            conta.cliente = gabriela;
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 147885;
+
+            Console.WriteLine(conta);
+            Console.WriteLine(conta.cliente.nome);
+            Console.WriteLine(conta.cliente.cpf);
+            Console.WriteLine(conta.saldo);
+
+
+
+        }
+    }
+}
