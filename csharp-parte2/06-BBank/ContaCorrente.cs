dcdd4ab3f1112995876b296b05ffe721718c4e16
taohansens <taohansens@gmail.com>
2021-05-30 19:59:22 -0300

    Aula 05. Encapsulamento

---
+++ csharp-parte2/06-BBank/ContaCorrente.cs
@@ -0,0 +1,43 @@
+﻿using _06_BBank;
+public class ContaCorrente {
+
+    private Cliente _titular;
+    private int _agencia;
+    private int _numero;
+    private double _saldo = 100;
+
+    public Cliente Cliente { get => _titular; set => _titular = value; }
+    public int Agencia { get => _agencia; set => _agencia = value; }
+    public int Numero { get => _numero; set => _numero = value; }
+    
+    public double Saldo {
+        get 
+            { return _saldo; }
+        set {
+            if (value < 0) {
+                return;
+            }
+        }
+    }
+
+    public bool Sacar(double valor) {
+        if (_saldo < valor) {
+            return false;
+        }
+        _saldo -= valor;
+        return true;
+    }
+
+    public void Depositar(double valor) {
+        _saldo += valor;
+    }
+
+    public bool Transferir(double valor, ContaCorrente contaDestino) {
+        if (_saldo < valor) {
+            return false;
+        }
+        _saldo -= valor;
+        contaDestino.Depositar(valor);
+        return true;
+    }
+}
---
+++ csharp-parte2/06-BBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06_BBank {
+    class Program {
+
+        static void Main(string[] args) {
+            string brkl = "==================";
+
+            Cliente c1 = new Cliente();
+            c1.Nome = "Gabriela";
+            c1.Profissao = "Estudante";
+            c1.Cpf = "342.334.232-23";
+
+            ContaCorrente conta = new ContaCorrente();
+            conta.Agencia = 3234;
+            conta.Cliente = c1;
+            conta.Numero = 445;
+            conta.Saldo = 150;
+
+            conta.Depositar(10);
+            Console.WriteLine(conta.Saldo);
+            Console.WriteLine(conta.Cliente.Nome);
+
+
+
+        }
+    }
+}
