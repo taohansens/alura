5489f476caf53969718672a674849461aac3532d
taohansens <taohansens@gmail.com>
2021-07-04 18:36:15 -0300

    Introdução Ao REGEX

--- csharp-parte6/ByteBank.SistemaAgencia/Program.cs
+++ csharp-parte6/ByteBank.SistemaAgencia/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
@@ -11,14 +12,28 @@ namespace ByteBank.SistemaAgencia
     class Program
     {
         static void Main(string[] args) {
-            
+            // REGEX
+            string texto = "O número da casa é 403 e o código é 1.2-4 e ok";
+            string pattern = "[0-9][.][0-9][-][0-9]";
+            Console.WriteLine(Regex.IsMatch(texto, pattern)); // Retorna True ou False
+            Match resultado = Regex.Match(texto, pattern);
+            Console.WriteLine(resultado);
+            Console.WriteLine("=======");
+
+            string texto2 = "O celular é 84334-2394, me ligue.";
+            string padrao = "[0-9]{4,5}-?{0,1}[0-9]{4}"; // Regex para _4 até 5 numeros_ - _4 numeros_ com hífen ou sem hífen;
+            Match resultado2 = Regex.Match(texto2, padrao);
+            Console.WriteLine(resultado2);
+
+
+
             // string urlTest = "http://www.bytebank.com/cambio";
             // int indiceByteBank = urlTest.IndexOf("http://www.bytebank.com");
             // urlTest.StartsWith("http://www.bytebank.com");
             // urlTest.EndsWith("cambio");
             // urlTest.Contains("bytebank");
 
-            
+
 
             // string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
             // ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
@@ -31,8 +46,8 @@ namespace ByteBank.SistemaAgencia
             // int indiceEComercial = testeRemocao.IndexOf('&');
             // Console.WriteLine(testeRemocao.Remove(indiceEComercial));
             // Console.ReadLine();
-            
-            
+
+
             // <nome> = <valor>
             // string palavra = "moedaOrigem=real&moedaDestino=dolar";
             // string nomeArgumento = "moedaDestino";
