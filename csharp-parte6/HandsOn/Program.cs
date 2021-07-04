c3a64c264aec861e94328344bc3bc04c08bb2f3a
taohansens <taohansens@gmail.com>
2021-07-04 10:11:32 -0300

    Aula06-Parte1

---
+++ csharp-parte6/HandsOn/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HandsOn {
+    class Program {
+        public static void Main(string[] args) {
+            Parte1();
+        }
+
+        private static void Parte1() {
+            var url = "pagina?argumentos";
+            var url2 = url.Substring(7);
+            Console.WriteLine(url2);
+        }
+    }
+}
---
+++ csharp-parte6/SistemaAgencia/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SistemaAgencia {
+    class Program {
+        static void Main(string[] args) {
+            
+            var url = "pagina?argumentos";
+            Console.WriteLine(url);
+            
+            var url2 = url.Substring(7);
+            
+            Console.WriteLine(url2);
+
+            Console.ReadLine();
+        }
+    }
+}
